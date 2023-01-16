bdfda921d8654bd0cf00317b9bd4e2a07075e20d
Joe Holmes <jholmes@quantic.edu>
2023-01-16 18:22:47 -0500

    first commit of content-sync plugin

--- main.ts
+++ main.ts
@@ -1,89 +1,114 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	TFile,
+} from "obsidian";
+const path = require("path");
+const fs = require("fs");
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface ContentSyncSettings {
+	pathToContentFolder: string;
+	contentKey: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const DEFAULT_SETTINGS: ContentSyncSettings = {
+	pathToContentFolder: path.join(
+		process.env.HOME,
+		"path",
+		"to",
+		"content",
+		"folder"
+	),
+	contentKey: "publish_to_content_folder",
+};
+export default class ContentSyncPlugin extends Plugin {
+	settings: ContentSyncSettings;
 
 	async onload() {
 		await this.loadSettings();
+		const { vault } = this.app;
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+		async function getFilesToPublish(frontmatterKey: string) {
+			const filesToPublish = await Promise.all(
+				vault
+					.getMarkdownFiles()
+					.filter(
+						(file) =>
+							app.metadataCache.getFileCache(file)?.frontmatter?.[
+								frontmatterKey
+							]
+					)
+			);
+			return filesToPublish;
+		}
+		const basePath = (this.app.vault.adapter as any).basePath;
+		async function getVaultPath(file: TFile) {
+			return path.join(basePath, file.parent.path, file.path);
+		}
+		async function syncFiles(settings: ContentSyncSettings) {
+			const { pathToContentFolder, contentKey } = settings;
+			const filesToPublish = await getFilesToPublish(contentKey);
+			filesToPublish.forEach(async (file) => {
+				const vaultPath = await getVaultPath(file);
+				const destinationPath = path.join(
+					pathToContentFolder,
+					file.path
+				);
+				if (
+					!fs.existsSync(destinationPath) ||
+					fs.statSync(vaultPath).mtimeMs >
+						fs.statSync(destinationPath)?.mtimeMs
+				) {
+					try {
+						if (!fs.existsSync(destinationPath)) {
+							new Notice(`adding ${file.path}`);
+						} else {
+							new Notice(`updating ${file.path}`);
+						}
+						fs.copyFileSync(vaultPath, destinationPath);
+					} catch (err) {
+						new Notice(`Error: ${err}`);
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
 				}
-			}
+				// now we check if any files exist in the destination folder that don't exist in filesToPublish
+				const filesInDestinationFolder =
+					fs.readdirSync(pathToContentFolder);
+				filesInDestinationFolder.forEach((file: string) => {
+					const fileExists = filesToPublish.find(
+						(f) => f.path === file
+					);
+					if (!fileExists) {
+						new Notice(`removing ${file}`);
+						fs.unlinkSync(path.join(pathToContentFolder, file));
+					}
+				});
+			});
+		}
+
+		this.addCommand({
+			id: "sync-files-with-content-folder",
+			name: "Sync files with content folder",
+			callback: async () => {
+				await syncFiles(this.settings);
+				return new Notice(`Sync complete`);
+			},
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new ContentSyncSettingTab(this.app, this));
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
@@ -91,47 +116,51 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class ContentSyncSettingTab extends PluginSettingTab {
+	plugin: ContentSyncPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: ContentSyncPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl("h2", {
+			text: "Setting up your sync to external content folder.",
+		});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Path to content folder")
+			.setDesc(
+				"Path to the folder where you want to copy over selected files."
+			)
+			.addText((text) =>
+				text
+					.setPlaceholder("Path here")
+					.setValue(this.plugin.settings.pathToContentFolder)
+					.onChange(async (value) => {
+						this.plugin.settings.pathToContentFolder = value;
+						await this.plugin.saveSettings();
+					})
+			);
+		containerEl.createEl("h2", {
+			text: "Frontmatter key that indicates a file should be synced.",
+		});
+		new Setting(containerEl)
+			.setName("Frontmatter key")
+			.setDesc("Value should be set to `true` to copy file over.")
+			.addText((text) =>
+				text
+					.setPlaceholder("Key here")
+					.setValue(this.plugin.settings.contentKey)
+					.onChange(async (value) => {
+						this.plugin.settings.contentKey = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
